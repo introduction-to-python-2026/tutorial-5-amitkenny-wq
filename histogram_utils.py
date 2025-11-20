a1b0ad3a257f97c69006bc970b38c08c0a03a9e7
amitkenny-wq <amit.kenny@gmail.com>
2025-11-20 13:27:54 +0200

    Fix indentation in build_histogram function

--- histogram_utils.py
+++ histogram_utils.py
@@ -1,13 +1,13 @@
 # Q: Write an import statement that imports the function pyplot from the module matplotlib and renames it to plt.
 import matplotlib.pyplot as plt
 def build_histogram(data):
-  my_dict = {}
-  for item in data:
-    if item in my_dict.keys():
-      my_dict[item] += 1
-    else:
-      my_dict.update({item:1})
-  return my_dict
+    my_dict = {}
+    for item in data:
+      if item in my_dict.keys():
+        my_dict[item] += 1
+      else:
+        my_dict.update({item:1})
+    return my_dict
 
 def plot_histogram(histogram):
     import matplotlib.pyplot as plt
